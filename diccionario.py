558153a3fc5cd4703e4e3525ce8e8f8d22bb150a
Madmaxronin <maxwellferrer1992@gmail.com>
2022-07-26 00:05:04 +0100

    first commit

---
+++ diccionario.py
@@ -0,0 +1,12 @@
+elementos = {"a": 1, "b": 2, "c": 3, "d": 4}
+
+"""
+elementos["nombre"] = "Cody" # Crea la llave con su valor
+elementos[(1, 2, 3)] ="La llave es una tupla"
+
+# Actualiza el valor de la llave
+elementos["nombre"] = "Max"
+"""
+
+print(elementos)
+print(len(elementos))
